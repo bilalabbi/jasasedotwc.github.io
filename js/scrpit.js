a4486ac1205ed026eee7aa677c2c14b0d70b2a63
bilalabbi <bilal.abbi27@gmail.com>
2024-12-19 22:43:25 +0700

    first commit

---
+++ js/scrpit.js
@@ -0,0 +1,6 @@
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  const windowPosition = window.scrollY > 0;
+  navbar.classList.toggle("scrolling-active", windowPosition);
+});
